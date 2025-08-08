d0c4ba6ee4df20865c199e00fdcb3044365a7dd2
dipranjanda <119164885+dipranjanda@users.noreply.github.com>
2025-08-08 23:35:19 +0530

    Update xss.py

--- vulnerabilities/xss.py
+++ vulnerabilities/xss.py
@@ -3,8 +3,5 @@ def test_xss(url):
     payload = "<script>alert('XSS')</script>"
     # scanning logic here
 
-# sqli.py
-def test_sqli(url):
-    payload = "' OR '1'='1"
-    # scanning logic here
+
 

a4e80a55f2512c832c23dca79fbe5794f8d4dfcb
dipranjanda <119164885+dipranjanda@users.noreply.github.com>
2025-08-08 23:34:36 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,35 +1,8 @@
-import argparse
-import requests
-from bs4 import BeautifulSoup
+from vulnerabilities import sqli, xss
 
-def check_security_headers(url):
-    try:
-        response = requests.get(url)
-        headers = response.headers
-
-        print("\n[+] Checking Security Headers...")
-        important_headers = [
-            "Content-Security-Policy",
-            "X-Content-Type-Options",
-            "Strict-Transport-Security",
-            "X-Frame-Options",
-            "X-XSS-Protection"
-        ]
-        for header in important_headers:
-            if header in headers:
-                print(f"[OK] {header} found")
-            else:
-                print(f"[MISSING] {header} is not set")
-    except requests.exceptions.RequestException as e:
-        print(f"[ERROR] Could not connect: {e}")
-
-def main():
-    parser = argparse.ArgumentParser(description="OWASP Basic Security Scanner")
-    parser.add_argument("-u", "--url", required=True, help="Target URL to scan")
-    args = parser.parse_args()
-
-    print(f"[*] Starting scan on {args.url}")
-    check_security_headers(args.url)
+def main(url):
+    sqli.test_sqli(url)
+    xss.test_xss(url)
 
 if __name__ == "__main__":
-    main()
+    main("http://example.com")
